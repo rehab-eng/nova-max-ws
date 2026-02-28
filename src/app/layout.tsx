1d6959d5c0e279f460687c65dc03951ed6f0164c
Mohamed <hsjshdvehhs@gmail.com>
2026-02-28 03:05:52 +0200

    chore: set metadata base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://nova-max-ws.pages.dev"),
   title: {
     default: "Nova Max | ???? ???? ??????",
     template: "%s | Nova Max",
